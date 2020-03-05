a98e68418560a889a6c47d5bfdc0453f9954b5d2
storku <36249562+storku@users.noreply.github.com>
2020-03-04 16:03:32 -0800

    refactor multinomial-logistic-regression for image recognition

--- regressions/multinomial-logistic-regression/index.js
+++ regressions/multinomial-logistic-regression/index.js
@@ -1,38 +1,34 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
-const loadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
 const plot = require("node-remote-plot");
 const _ = require("lodash");
+const mnist = require("mnist-data");
 
-let { features, labels, testFeatures, testLabels } = loadCSV(
-  "../data/cars.csv",
-  {
-    dataColumns: ["horsepower", "displacement", "weight"],
-    labelColumns: ["mpg"],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-      mpg: value => {
-        const mpg = parseFloat(value);
+const mnistData = mnist.training(0, 60000);
 
-        if (mpg < 15) {
-          return [1, 0, 0];
-        } else if (mpg < 30) {
-          return [0, 1, 0];
-        } else {
-          return [0, 0, 1];
-        }
-      }
-    }
-  }
-);
+const features = mnistData.images.values.map(image => _.flatMap(image));
+const encodedLabels = mnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-  learningRate: 0.5,
-  iterations: 100,
-  batchSize: 10
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 20,
+  batchSize: 100
 });
 
 regression.train();
-console.log(regression.test(testFeatures, _.flatMap(testLabels)));
+
+const testMnistData = mnist.testing(0, 1000);
+const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
+const testEncodedLabels = testMnistData.labels.values.map(label => {
+  const row = new Array(10).fill(0);
+  row[label] = 1;
+  return row;
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log({ accuracy });
